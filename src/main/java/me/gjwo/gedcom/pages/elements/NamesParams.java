ea9655184076158c0aaf004b44671807a9518fb0
Graham Wood <grahamjwood@gmail.com>
2018-04-16 17:44:45 +0100

    Copyright notices added to source files

--- src/main/java/me/gjwo/gedcom/pages/elements/NamesParams.java
+++ src/main/java/me/gjwo/gedcom/pages/elements/NamesParams.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018.     GedcomView generates live web pages from a .ged file
+ *                                Copyright (C) 2018  Graham J Wood
+ *
+ *                                This program is free software: you can redistribute it and/or modify
+ *                                it under the terms of the GNU General Public License as published by
+ *                                the Free Software Foundation, either version 3 of the License, or
+ *                                (at your option) any later version.
+ *
+ *                                This program is distributed in the hope that it will be useful,
+ *                                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                                GNU General Public License for more details.
+ *
+ *                                You should have received a copy of the GNU General Public License
+ *                                along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package me.gjwo.gedcom.pages.elements;
 
 import org.gedcom4j.model.Individual;
