b4a689aefe6bc4e23413ef58a01c5abb42a6bc7b
Graham Wood <grahamjwood@gmail.com>
2018-04-18 14:56:53 +0100

    Sources index displays as sorted table, restructured to match other code

---
+++ src/main/java/me/gjwo/gedcom/SourceBuilder.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2018.     GedcomView generates live web pages from a .ged file
+ *                                Copyright (C) 2018  Graham J Wood
+ *
+ *                                This program is free software: you can redistribute it and/or modify
+ *                                it under the terms of the GNU General Public License as published by
+ *                                the Free Software Foundation, either version 3 of the License, or
+ *                                (at your option) any later version.
+ *
+ *                                This program is distributed in the hope that it will be useful,
+ *                                but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                                GNU General Public License for more details.
+ *
+ *                                You should have received a copy of the GNU General Public License
+ *                                along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package me.gjwo.gedcom;
+
+import org.gedcom4j.model.Source;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.Integer.parseInt;
+
+public class SourceBuilder
+{
+    private final Map<String,Source> sources;
+    public SourceBuilder(Map<String,Source> sources)
+    {
+        this.sources = sources;
+    }
+    public List<List<String>> buildSourcesTable()
+    {
+        List<List<String>> tableRows = new ArrayList<>();
+        for (String key :sources.keySet())
+            tableRows.add(buildSourcesTableRow(key,sources.get(key)));
+        tableRows.sort((o1, o2) -> {
+            try {
+                int i1, i2;
+                i1 = parseInt(o1.get(0));
+                i2 = parseInt(o2.get(0));
+                return i1 - i2;
+            } catch (Exception e) {
+                return 0;
+            }
+        });
+        return tableRows;
+    }
+    private List<String> buildSourcesTableRow(String key, Source source)
+    {
+        List<String> row = new ArrayList<>();
+        row.add(key.replace("@","").replace("S",""));
+        row.add(getTitle(source));
+        row.add(getAuthor(source));
+        row.add(getNoteStructLine(source));
+        return row;
+    }
+    public List<String> buildColumnLables()
+    {
+        List<String> row = new ArrayList<>();
+        String content = "";
+        row.add("Ref");
+        row.add("Title");
+        row.add("Author");
+        row.add("Source note");
+        return row;
+    }
+    private String getNoteStructLine(Source source)
+    {
+        if(source!=null)
+            if(source.getNoteStructures()!=null)
+                if(source.getNoteStructures().get(0)!=null)
+                    if(source.getNoteStructures().get(0).getLines()!=null)
+                        if(source.getNoteStructures().get(0).getLines().get(0)!=null)
+                            return source.getNoteStructures().get(0).getLines().get(0);
+                        else return "";
+                    else return "";
+                else return "";
+            else return "";
+        else return "";
+    }
+    private String getTitle(Source source)
+    {
+        if(source!=null)
+            if(source.getTitle()!=null)
+                if(source.getTitle().getLines()!=null)
+                    if(source.getTitle().getLines().get(0)!=null)
+                        return source.getTitle().getLines().get(0);
+                    else return "";
+                else return "";
+            else return "";
+        else return "";
+    }
+    private String getAuthor(Source source)
+    {
+        if(source!=null)
+            if(source.getOriginatorsAuthors()!=null)
+                if(source.getOriginatorsAuthors().getLines()!=null)
+                    if(source.getOriginatorsAuthors().getLines().get(0)!=null)
+                        return source.getOriginatorsAuthors().getLines().get(0);
+                    else return "";
+                else return "";
+            else return "";
+        else return "";
+    }
+}
